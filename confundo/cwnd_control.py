a840e2d0937f2f21d7d9f8a73433ca8feb21c24b
Gallomond <goldenlemay@gmail.com>
2023-12-03 09:49:41 -0800

    update

--- confundo/cwnd_control.py
+++ confundo/cwnd_control.py
@@ -1,27 +1,28 @@
 # -*- Mode: python; py-indent-offset: 4; indent-tabs-mode: nil; coding: utf-8; -*-
 # Copyright 2019 Alex Afanasyev
-#
 
 from .common import *
 
 class CwndControl:
-    '''Interface for the congestio control actions'''
+    '''Implementation of TCP Tahoe congestion control'''
 
     def __init__(self):
         self.cwnd = 1.0 * MTU
-        self.ssthresh = INIT_SSTHRESH
+        self.ss_thresh = INIT_SSTHRESH
 
     def on_ack(self, ackedDataLen):
-        #
-        # IMPLEMENT this and call this method in approprite place inside confundo/socket.py
-        #
-        pass
+        # Slow start
+        if self.cwnd < self.ss_thresh:
+            self.cwnd += 412
+        # Congestion avoidance
+        elif self.cwnd >= self.ss_thresh:
+            self.cwnd += (412 * 412) / self.cwnd
 
     def on_timeout(self):
-        #
-        # IMPLEMENT this and call this method in approprite place inside confundo/socket.py
-        #
-        pass
+        # Timeout handling
+        self.ss_thresh = self.cwnd / 2
+        self.cwnd = 412  # Set to the initial value
+        # Retransmit data after the last acknowledged byte (not implemented here)
 
     def __str__(self):
-        return f"cwnd:{self.cwnd} ssthreash:{self.ssthresh}"
+        return f"cwnd:{self.cwnd} ss_thresh:{self.ss_thresh}"
