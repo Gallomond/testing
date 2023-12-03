eb94baee4e51516b69ee5f2d43f8d5ed9b7abcde
Gallomond <goldenlemay@gmail.com>
2023-12-03 10:37:07 -0800

    update client.py

--- client.py
+++ client.py
@@ -27,6 +27,21 @@ def start():
                         sent = sock.send(data[total_sent:])
                         total_sent += sent
                         data = f.read(50000)
+
+             # Gracefully terminate the connection
+            sock.sendFinPacket()
+            sock.expectFinAck()
+
+            # Wait for incoming FIN packets
+            start_time = time.time()
+            while time.time() - start_time < 2:
+                pkt = sock._recv()
+                if pkt and pkt.isFin:
+                    # Respond to each incoming FIN with an ACK packet
+                    sock.sendAckPacket()
+
+            # Close connection
+            sock.close()
     except RuntimeError as e:
         sys.stderr.write(f"ERROR: {e}\n")
         sys.exit(1)
